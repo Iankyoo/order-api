236c24f4639ad6ecf0ad09d2617dc8958b27d247
Iankyoo <contato.iankyoo@gmail.com>
2026-06-18 21:39:05 -0300

    feat: add JwtAuthFilter

--- src/main/java/com/iankyoo/orderapi/order_api/config/SecurityConfig.java
+++ src/main/java/com/iankyoo/orderapi/order_api/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.iankyoo.orderapi.order_api.config;
 
+import com.iankyoo.orderapi.order_api.filter.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,10 +10,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter){
+        this.jwtAuthFilter= jwtAuthFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -29,7 +37,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/menuitens/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
